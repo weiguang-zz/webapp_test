a0c7ab122c1fcee72c80c7e9b1fa4559117f1bb7
zheng <zhengzhandqingbin@gmail.com>
2025-12-04 21:04:11 +0800

    fix

--- server/server.py
+++ server/server.py
@@ -48,8 +48,7 @@ async def handle_join(websocket, payload, current_room_id):
     user_info = payload.get('userInfo', {})
     
     if not room_id:
-        return current_room_id、
-
+        return current_room_id
 
     # Leave previous room if any
     if current_room_id:
